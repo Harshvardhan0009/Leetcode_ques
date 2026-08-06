722535293657c7e38846129736168d80f13081a0
Harshvardhan Sharma <148997077+Harshvardhan0009@users.noreply.github.com>
2026-08-06 10:58:14 +0530

    Time: 797 ms (5.45%), Space: 324.5 MB (47.62%) - LeetHub

---
+++ 3310-remove-methods-from-project/3310-remove-methods-from-project.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<int> remainingMethods(int n, int k, vector<vector<int>>& invocations) {
+        vector<vector<int>> graph(n);
+        for (auto &e : invocations) {
+            graph[e[0]].push_back(e[1]);
+        }
+        vector<bool> suspicious(n, false);
+        function<void(int)> dfs = [&](int node) {
+            suspicious[node] = true;
+            for (int next : graph[node]) {
+                if (!suspicious[next]) {
+                    dfs(next);
+                }
+            }
+        };
+        dfs(k);
+        for (auto &e : invocations) {
+            int u = e[0], v = e[1];
+            if (!suspicious[u] && suspicious[v]) {
+                vector<int> ans;
+                for (int i = 0; i < n; i++)
+                    ans.push_back(i);
+                return ans;
+            }
+        }
+        vector<int> ans;
+        for (int i = 0; i < n; i++) {
+            if (!suspicious[i])
+                ans.push_back(i);
+        }
+        return ans;
+    }
+};
