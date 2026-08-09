0c667da1d9a16ac6e19d2407d9dc9705a366f4de
Harshvardhan Sharma <148997077+Harshvardhan0009@users.noreply.github.com>
2026-08-09 18:08:03 +0530

    Time: 361 ms (17.13%), Space: 0B (100%) - LeetHub

---
+++ 0610-triangle-judgement/0610-triangle-judgement.sql
@@ -0,0 +1,12 @@
+SELECT
+    x,
+    y,
+    z,
+    CASE
+        WHEN x + y > z
+         AND x + z > y
+         AND y + z > x
+        THEN 'Yes'
+        else 'No'
+    end as triangle
+from triangle;
