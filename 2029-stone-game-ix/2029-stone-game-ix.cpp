d22f51206837b0d51db9d5ce8a2e58c2d658a281
Harshvardhan Sharma <148997077+Harshvardhan0009@users.noreply.github.com>
2026-08-17 00:19:31 +0530

    Time: 8 ms (30.2%), Space: 131.4 MB (33.5%) - LeetHub

--- 2029-stone-game-ix/2029-stone-game-ix.cpp
+++ 2029-stone-game-ix/2029-stone-game-ix.cpp
@@ -1,30 +1,14 @@
 class Solution {
 public:
-
-    bool check(vector<int> cnt) {
-        if (cnt[1] == 0)
-            return false;
-
-        cnt[1]--;
-
-        int turns = 1 + min(cnt[1], cnt[2]) * 2 + cnt[0];
-
-        if (cnt[1] > cnt[2]) {
-            cnt[1]--;
-            turns++;
-        }
-
-        return turns % 2 == 1 && cnt[1] != cnt[2];
-    }
-
     bool stoneGameIX(vector<int>& stones) {
-        vector<int> cnt(3, 0);
+        int cnt[3] = {};
 
         for (int x : stones)
             cnt[x % 3]++;
 
-        vector<int> swapped = {cnt[0], cnt[2], cnt[1]};
+        if (cnt[0] % 2 == 0)
+            return min(cnt[1], cnt[2]) > 0;
 
-        return check(cnt) || check(swapped);
+        return abs(cnt[1] - cnt[2]) > 2;
     }
 };
